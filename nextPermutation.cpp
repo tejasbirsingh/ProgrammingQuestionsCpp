9b08922b9edb4b53174092139203420f3b9b6f56
Tejas Bir Singh <tejasbir.rekhi@gmail.com>
2020-08-21 15:02:54 +0530

    Add files via upload

---
+++ nextPermutation.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    void nextPermutation(vector<int>& nums) {
+        
+    int flag=-1,m=0,k=0;
+	
+	for(int i=nums.size()-1;i>0;i--){
+        
+		if(nums[i]>nums[i-1]){
+			flag=i-1;
+			break;
+		}
+	}
+
+	if(flag==-1){
+		sort(nums.begin(),nums.end());
+		return;
+	}
+
+	for(k=nums.size()-1;k>flag;k--)
+	{
+		if(nums[k]>nums[flag])
+			{
+			m=k;
+			break;
+		}
+	}
+	swap(nums[flag],nums[m]);
+
+	sort(nums.begin()+flag+1,nums.end());}
+
+
+
+int main(){
+
+	vector<int> arr={4 ,1 ,5 ,3 ,2};
+	nextPermutation(arr);
+	for(auto i:arr){
+		cout<<i;
+	}
+	return 0;
+}
